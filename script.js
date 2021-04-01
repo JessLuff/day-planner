c8749bb483b7d4120b4b9cba24ee75116ea9268e
Jessica Luff <78363096+JessLuff@users.noreply.github.com>
2021-04-01 22:59:06 +1100

    One slot functional

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+var today = moment();
+$("#currentDay").text(today.format('dddd, Do [of] MMMM YYYY'));
+
 var slot9 = $('#slot9');
 var text9 = $('#todo9');
-text9.attr('placeholder', localStorage.getItem('todo9'));
-text9.append(text9);
+text9.attr('value', localStorage.getItem('todo9'));
+if (today.format('h')<9){
+    text9.attr('class', "bg-success text-white");
+} else if (today.format('h')==9){
+    text9.attr('class', "bg-danger text-white");
+} else {
+    text9.attr('class', "bg-secondary text-white");
+}
 
-function handleFormSubmit(event) {
+function button9(event) {
     event.preventDefault();
     var todo9 = $('input[name="todo9"]').val();
     localStorage.setItem('todo9', todo9);
 }
 
-slot9.on('submit', handleFormSubmit);
-
+slot9.on('submit', button9);
